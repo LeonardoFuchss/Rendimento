8986d1750242d5a68ef91dc927c72f1c787057b2
Leonardo Fuchs <Leonardo.santosfuchs07@gmail.com>
2023-05-17 13:33:18 -0300

    atualização

--- Poupanca/src/Rendimento.java
+++ Poupanca/src/Rendimento.java
@@ -1,29 +1,54 @@
 import java.util.Scanner;
+
 public class Rendimento {
     //propriedades
     private double valor;
     private int quantMeses;
     private double taxaSelic;
-    double taxaRef;
+    private double taxaRef;
+    private Scanner scanner = new Scanner(System.in);
+
 
     //Construtor com valor padrão que irei definir e com o scanner para a interação com o usuário.
-    public Rendimento(double taxaSelic, double taxaRef, Scanner scanner) {
+    public Rendimento(double taxaSelic, double taxaRef) {
         this.taxaSelic = taxaSelic;
         this.taxaRef = taxaRef;
+    }
+    public void interacaoUsuario(){
         System.out.println("Olá, informe o valor que queira investir: ");
         this.valor = scanner.nextDouble();
         System.out.println("Agora, informe a quantidade de meses que queira deixar rendendo: ");
         this.quantMeses = scanner.nextInt();
-
     }
-    public void calcularRendimento() {
 
+
+    public double getValor() {
+        return this.valor;
+    }
+
+    public int getQuantMeses() {
+        return this.quantMeses;
+    }
+
+
+    public double calculaRendimento() {
+        double rendimento;
         if (this.taxaSelic < 8.5) {
-            System.out.println("O seu rendimento em " +quantMeses+ " meses foi de: " +(valor + (0.70 * taxaSelic + taxaRef) * quantMeses));
+            rendimento = valor + (0.70 * taxaSelic + taxaRef) * quantMeses;
         } else {
-            System.out.println("O seu rendimento em " +quantMeses+ " meses foi de: " +(valor + (0.05 * taxaSelic + taxaRef) * quantMeses));
+            rendimento = valor + (0.05 * taxaSelic + taxaRef) * quantMeses;
+
         }
+        return rendimento;
+    }
+
+    public void mesAmes() {
+        double rendimentoMensal = ((calculaRendimento() - getValor()) / getQuantMeses());
+        System.out.println("O seu rendimento final foi de " + calculaRendimento());
+        System.out.println("E o seu rendimento mensal foi de: " + rendimentoMensal);
     }
 }
+
+
 
 
--- Poupanca/src/teste.java
+++ Poupanca/src/teste.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class teste {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Rendimento rendimento = new Rendimento(7.5, 5.0, scanner);
-        rendimento.calcularRendimento();
+        Rendimento rendimento = new Rendimento(7.5, 5.0);
+        rendimento.interacaoUsuario();
+        rendimento.mesAmes();
     }
 }
